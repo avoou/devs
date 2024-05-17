a4950c6f1f8683f95ce6dee01f3ba8bcb62291cd
avoou <xxxenodochialritchie@gmail.com>
2024-05-17 18:34:12 +0300

    add uploading files

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -121,6 +121,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# STATICFILES_DIR = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+MEDIA_URL = ''
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -8,11 +8,13 @@ import uuid
 
 
 class Project(models.Model):
+    
     tag = models.ManyToManyField('Tag', blank=True)
     vote_total = models.IntegerField(default=0, null=True, blank=True)
     vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
+    featured_image = models.ImageField(null=True, blank=True, default='default.png')
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
