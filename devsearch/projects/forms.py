23579d377258d9c7b3973fb832da9f1ac318dec9
avoou <xxxenodochialritchie@gmail.com>
2024-05-20 19:17:25 +0300

    update templates

--- devsearch/projects/forms.py
+++ devsearch/projects/forms.py
@@ -1,7 +1,17 @@
+from typing import Any, Mapping
+from django.core.files.base import File
+from django.db.models.base import Model
 from django.forms import ModelForm
+from django.forms.utils import ErrorList
 from .models import Project
+from django import forms
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        for _, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input input--text'})
