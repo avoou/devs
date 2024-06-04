ba5ebdcdda44eacb864225180a0669d41c42e27e
avoou <xxxenodochialritchie@gmail.com>
2024-06-04 11:21:50 +0300

    update form

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 from .models import Profile
 from django.contrib import messages
 
 
 def registerUser(request):
     page = 'register'
-    form = UserCreationForm()
+    form = CustomUserCreationForm()
     context = {'page': page, 'form': form}
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             print('usercreationform is valid')
             user = form.save(commit=False)
