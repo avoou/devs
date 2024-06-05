fb14b01377d4fca76a9eac68f9b7dc47298bdd6f
avoou <xxxenodochialritchie@gmail.com>
2024-06-05 18:41:44 +0300

    update register page design

--- devsearch/users/forms.py
+++ devsearch/users/forms.py
@@ -6,3 +6,8 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        for _, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input input--text'})
